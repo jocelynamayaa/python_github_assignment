eda94c08fa353040ce16ae26ae7e2f967de83d5b
Jocelyn Amaya-Gibson <jocelyn280@usf.edu>
2025-11-23 01:02:49 -0500

    I added a formatted output to display how much they would save in a year at the rate they are saving monthly.

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ savings = input("How much money did you save this month? ")
 
 savings = float(savings)
 yearly_savings = savings * 12
+
+print(f"At this saving rate, you will save ${yearly_savings:.2f} in a year.")
