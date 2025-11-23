e87deba09800fc4a2fd6f12a23f0709770af2780
Jocelyn Amaya-Gibson <jocelyn280@usf.edu>
2025-11-23 01:37:20 -0500

    Cleans up my code so that there is no duplicates.

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-print("Welcome to my Python program!") # Task 1 - Welcome Message
+print("Welcome to my Python program!") # Welcome Message
 
-savings = input("How much money did you save this month? ") # Task 2 - Asks the user for input
-
-savings = float(savings) # Task 3 - Converts input to float
-yearly_savings = savings * 12 # Performs calculation for the year
-
-print(f"At this saving rate, you will save ${yearly_savings:.2f} in a year.") # Task 4 - Displays the result clearly
-
-# Task 5 - Error handling for non-numeric input
+# Error handling for non-numeric input
 try:
     saving = float(input("How much money did you save this month? "))
 except ValueError: 
     print("Please enter a valid number.")
     exit()
+
+yearly_savings = saving * 12 # Calculates yearly savings
+
+print(f"At this saving rate, you will save ${yearly_savings:.2f} in a year.") # Displays the result clear
