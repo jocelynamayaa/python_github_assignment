f04dda96d1b49126d24445235024457912e0d4c7
Jocelyn Amaya-Gibson <jocelyn280@usf.edu>
2025-11-23 01:06:43 -0500

    Adds a basic error handling if the user does not put in a number.

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ savings = float(savings)
 yearly_savings = savings * 12
 
 print(f"At this saving rate, you will save ${yearly_savings:.2f} in a year.")
+
+try:
+    saving = float(input("How much money did you save this month? "))
+except ValueError: 
+    print("Please enter a valid number.")
+    exit()
